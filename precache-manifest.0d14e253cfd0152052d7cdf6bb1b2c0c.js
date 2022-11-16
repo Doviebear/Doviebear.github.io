7adda85ad08432fb412b1ef85c4c48b5315bb986
dshalev <dshalev@umich.edu>
2022-11-16 12:47:29 -0500

    Updates

---
+++ precache-manifest.0d14e253cfd0152052d7cdf6bb1b2c0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b6b39d0e0f0364972ea",
+    "url": "/static/css/main.06929d50.chunk.css"
+  },
+  {
+    "revision": "7b6b39d0e0f0364972ea",
+    "url": "/static/js/main.ef8a3255.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca8562f04f27eeb964b0",
+    "url": "/static/js/2.a77e7803.chunk.js"
+  },
+  {
+    "revision": "33e7dc01eb6aa28e241a2848119cd074",
+    "url": "/index.html"
+  }
+];
---
+++ static/js/main.ef8a3255.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{12:function(e,n,t){},13:function(e,n,t){},14:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o),l=t(2),a=t.n(l);t(12),t(13);var c=function(){return r.a.createElement("div",null,r.a.createElement("h1",null,"Hello World"))};a.a.createRoot(document.getElementById("root")).render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(c,null)))},3:function(e,n,t){e.exports=t(14)}},[[3,1,2]]]);
+//# sourceMappingURL=main.ef8a3255.chunk.js.map
